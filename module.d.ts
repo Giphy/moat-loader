edf85ac4581469f6ccca51f413dc0d5b7e288d03
cosmocochrane1 <cosmocochrane1@gmail.com>
2020-01-16 11:53:09 -0500

    moat tarball

---
+++ module.d.ts
@@ -0,0 +1,6 @@
+declare module 'moat-display-loader' {
+    export function loadMoatTag(partnerCode: string): Promise<any>;
+    export function startTracking(adElement: HTMLElement, adIdentifiers: object): number;
+    export function stopTracking(adNum: number);
+    export function stopTrackingAllAds();
+}
