2580b3cf9f3872aa9766ddbe605be6ac0c66be76
giannif <giannif@gmail.com>
2020-03-11 10:49:27 -0700

    moat-loader takes a url as an argument

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-/*Copyright (c) 2011, 2019, Oracle and/or its affiliates.  All rights reserved.*/
-function e() {
-  this.a = "moat-display-dynamic";
-}
-e.prototype.b = function(h) {
-  var k = this;
-  return new Promise(function(l, c) {
+/*Copyright (c) 2011, 2020, Oracle and/or its affiliates.  All rights reserved.*/
+function f() {}
+f.prototype.a = function(g, d) {
+  var h = this;
+  return new Promise(function(k, b) {
     "undefined" === typeof document &&
-      c("moat_npm_module: Moat tag doesn't support current environment");
-    var b = document.createElement("script"),
-      d = document.querySelector("head");
-    (b && d) || c("moat_npm_module: Moat tag can't be loaded");
+      b("moat_npm_module: Moat tag doesn't support current environment");
+    g || b("moat_npm_module: partnercode is required");
+    var c = document.createElement("script"),
+      e = document.querySelector("head");
+    (c && e) || b("moat_npm_module: Moat tag can't be loaded");
     var a = Math.round(Date.now() / 1e3),
-      m = a << 1;
+      l = a << 1;
     a =
       String.fromCharCode(77, 79, 65, 84) +
       window[String.fromCharCode(98, 116, 111, 97)](a);
-    window[a] = k;
-    window[a].resolve = l;
-    window[a].reject = c;
-    b.type = "text/javascript";
-    d.insertBefore(b, d.childNodes[0] || null);
-    b.src = "https://giphyscripts.s3.amazonaws.com/moat/moatad.js#" + m;
+    window[a] = h;
+    window[a].resolve = k;
+    window[a].reject = b;
+    window[a].adFindingMethod = "moat-display-dynamic";
+    window[a].moatTagHostedFullUrl = d;
+    c.type = "text/javascript";
+    e.insertBefore(c, e.childNodes[0] || null);
+    c.src = (d ? d : "https://z.moatads.com/" + g + "/moatad.js") + "#" + l;
   });
 };
-var f = new e(),
-  g = {};
-g.adFindingMethod = f.a;
-g.loadMoatTag = f.b;
-module.exports = g;
+module.exports = { loadMoatTag: new f().a };
